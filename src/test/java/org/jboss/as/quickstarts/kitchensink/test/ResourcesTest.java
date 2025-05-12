d8f5d145b5fd2d74ded06464f704c9f93cc2c35b
Obi-Dan <danshoff@gmail.com>
2025-05-12 17:00:30 +0100

    Add additional test for ResourcesTest

--- src/test/java/org/jboss/as/quickstarts/kitchensink/test/ResourcesTest.java
+++ src/test/java/org/jboss/as/quickstarts/kitchensink/test/ResourcesTest.java
@@ -1,6 +1,9 @@
 package org.jboss.as.quickstarts.kitchensink.test;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.logging.Logger;
 
 import org.jboss.as.quickstarts.kitchensink.util.Resources;
 import org.junit.Test;
@@ -15,5 +18,14 @@ public class ResourcesTest {
         // Simply verify that the Resources class can be instantiated
         Resources resources = new Resources();
         assertNotNull(resources);
+    }
+    
+    @Test
+    public void testLoggerCreation() {
+        // This is a basic test that doesn't rely on CDI injection
+        // Just verify we can create a logger directly
+        Logger logger = Logger.getLogger(ResourcesTest.class.getName());
+        assertNotNull(logger);
+        assertTrue(logger.getName().contains("ResourcesTest"));
     }
 } 
